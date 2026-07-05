4cf9fd34bb678bc9cf64be3345f143770d7db061
Dharshan <dharshan08260826@gmail.com>
2026-07-06 01:27:21 +0530

    build: dynamically set base path depending on whether build environment is Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/trading-journal/',
+  base: process.env.VERCEL ? '/' : '/trading-journal/',
   plugins: [react()],
   server: {
     port: 5173,
